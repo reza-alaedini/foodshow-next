115f18d4fc8591ac94c6e8b159c13be21793145f
Reza Alaedini <parsa.alaei.pa@gmail.com>
2023-07-16 16:21:09 +0330

    fetching data in dynamic menu page

---
+++ pages/menu/[id].js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+
+function Details({ data }) {
+  const router = useRouter();
+
+  console.log(data);
+
+  if (router.isFallback) {
+    return <h2>Loading the page...</h2>;
+  }
+
+  return <div>Details</div>;
+}
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({ params: { id: food.id.toString() } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { id },
+  } = context;
+  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+  };
+}
